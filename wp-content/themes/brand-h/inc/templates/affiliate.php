f47baff51e83e6fae5ebb3da1c5df64082861c38
ryancho <chowin21@gmail.com>
2020-08-30 11:00:48 +0900

    merge

--- wp-content/themes/brand-h/inc/templates/affiliate.php
+++ wp-content/themes/brand-h/inc/templates/affiliate.php
@@ -1,13 +1,13 @@
 
-<div class="pt-20">
-  <!-- <div id="popup_cancel_btn" class="absolute top-0 right-0 p-4 m-4 text-3xl cursor-pointer z-40 font-bold text-white hidden" >X</div>
-  <div id="pop_up_bg" class="absolute w-screen h-screen top-0 right-0 z-20 hidden" style="background-color: rgba(0, 0, 0, 0.5);"></div>
+<div class="pt-20 relative">
+  <div id="popup_cancel_btn" class="absolute top-0 right-0 p-4 m-4 text-3xl cursor-pointer z-40 font-bold text-white hidden" style="top:70px;" >X</div>
+  <div id="pop_up_bg" class="absolute w-screen-98 h-screen-per top-0 right-poop-0 z-20 hidden" style="background-color: rgba(0, 0, 0, 0.5);"></div>
   <div id="pop_up_bg2" class="absolute w-screen h-screen top-0 right-0 z-20 hidden" style="background-color: rgba(0, 0, 0, 0.5);"></div>
-  <div id="popup_cancel_btn2" class="h-screen h-full absolute top-0 right-0 p-4 m-4 text-3xl cursor-pointer z-40 font-bold text-white hidden" >X</div> -->
+  <div id="popup_cancel_btn2" class="h-screen h-full absolute top-0 right-0 p-4 m-4 text-3xl cursor-pointer z-40 font-bold text-white hidden" >X</div>
   
   <!--Hidden-popup better (start)  -->
-  <!-- <div id="popup" class="hidden z-40 absolute top-0 " style="top:25%" >
-    <div class="py-5 px-5 mx-5 bg-white flex flex-col rounded" style="z-index:1">  
+  <div id="popup" class="hidden z-40 absolute top-0 " style="top:25%" >
+    <div class="py-10 px-10 mx-5 bg-white flex flex-col rounded" style="z-index:1">  
       <div alt="hospital image"
                 class="bg-cover content-partners"
                 style="background-image: url(<?php bloginfo('template_directory') ?>/img/partners/01.jpg);">
@@ -29,11 +29,11 @@
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
   <!--Hidden-popup better (end)  -->
   <!--Hidden-popup better (start)  -->
-  <!-- <div id="popup2" class="hidden z-40 absolute top-0" style="top:15%" >
-    <div class="py-5 px-5 mx-5 bg-white flex flex-col" style="z-index:1">  
+  <div id="popup2" class="hidden z-40 absolute top-0" style="top:15%" >
+    <div class="py-10 px-10 mx-5 bg-white flex flex-col" style="z-index:1">  
       <div alt="hospital image"
                 class="bg-cover content-partners"
                 style="background-image: url(<?php bloginfo('template_directory') ?>/img/partners/02.jpg);">
@@ -53,11 +53,11 @@
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
   <!--Hidden-popup better (end)  -->
   <!--Hidden-popup better (start)  -->
-  <!-- <div id="popup3" class="hidden z-40 absolute top-0" style="top:15%" >
-    <div class="py-5 px-5 mx-5 bg-white flex flex-col" style="z-index:1">  
+  <div id="popup3" class="hidden z-40 absolute top-0" style="top:15%" >
+    <div class="py-10 px-10 mx-5 bg-white flex flex-col" style="z-index:1">  
       <div alt="hospital image"
                 class="bg-cover content-partners"
                 style="background-image: url(<?php bloginfo('template_directory') ?>/img/partners/03.jpg);">
@@ -82,11 +82,11 @@ Surgery: Interbody fusion, C-spine or L-spine, Posterior fusion, Total Knee Repl
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
   <!--Hidden-popup better (end)  -->
  <!--Hidden-popup better (start)  -->
- <!-- <div id="popup4" class="hidden z-40 absolute top-0" style="top:15%" >
-    <div class="py-5 px-5 mx-5 bg-white flex flex-col" style="z-index:1">  
+ <div id="popup4" class="hidden z-40 absolute top-0" style="top:15%" >
+    <div class="py-10 px-10 mx-5 bg-white flex flex-col" style="z-index:1">  
       <div alt="hospital image"
                 class="bg-cover content-partners"
                 style="background-image: url(<?php bloginfo('template_directory') ?>/img/partners/04.jpg);">
@@ -109,60 +109,48 @@ Surgery: Interbody fusion, C-spine or L-spine, Posterior fusion, Total Knee Repl
         </div>
       </div>
     </div>
-  </div> -->
+  </div>
   <!--Hidden-popup better (end)  -->
 
-  <!-- Popup by harsh -->
-          <div id="bms_popup" class="flex flex-col items-center p-4 m-4 sm:p-10 fixed hidden z-40 " style="max-width: 635px; height: 60vh; background-color: white; top: 50%;transform: translateY(-50%); max-height:100vh">
-            <div class="w-full overflow-hidden" style="height: 45%; flex:40%;">
-                <img id="bms_back_image" class="w-full bg-white" alt="" srcset="">
-            </div>
-            <div class="w-32 h-32 sm:w-40 sm:h-40 absolute border-solid border-4 sm:border-8 border-gray-600 rounded-full overflow-hidden" style="top:40%;transform: translateY(-60%);">
-                <img id="bms_avatar_image" class="w-full bg-white" alt="" srcset="">
-            </div>
-            <div class="w-full overflow-auto mt-24" style="flex:60%;">
-                <p id="bms_heading">Text</p>
-                <p id="bms_about_text">#Text</p>
-                <p id="bms_tags">#Text</p>
-            </div>
-        </div>
-        <div id="bms_popup_cancel_btn" class="fixed top-0 right-0 p-2 m-2 sm:p-4 sm:m-4 text-2xl sm:text-3xl cursor-pointer z-40 font-bold text-white hidden">X</div>
-        <div id="bms_pop_up_bg" class="fixed w-screen h-screen top-0 left-0 z-20 hidden" style="background-color: rgba(0, 0, 0, 0.5);"></div>
-  <!-- Pop Up End -->
 
-  <div>
-    <img class="mx-auto mb-24 mt-5 lg:h-24 h-24 w-24 lg:w-24"  src="<?php bloginfo('template_directory') ?>/img/hospital-alt-light.svg" >
+  <div style="margin-top:70px;">
+    
+    
+    <div class="border-gray-700 border w-16 h-16 rounded-full py-2 px-2 flex items-center justify-center text-gray-700" style="margin:0 auto">
+        <img class="mx-auto lg:h-8 h-8 w-8 lg:w-8"  src="<?php bloginfo('template_directory') ?>/img/hospital-alt-light.svg" >
+      </div>
+    
   </div>
   <div class="w-full mb-32 ">
-    <h2 class="font-bold text-center px-6 font-size-24 font-roboto leading-relaxed " style="font-size:22px">
+    <h2 class="font-bold text-center px-6 font-size-24 leading-relaxed " style="font-size:22px">
       Check out Korea’s <br class="lg:hidden">best medical staff.
     </h2>
   </div>
   <!-- logo --> 
-  <div id="best_medical_staffs" class="mt-20 ">
+  <div class="mt-20 main_cont_img">
     <div class="flex">
-      <img id="best_medical_staff_1"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/01-logo.png" >
-      <img id="best_medical_staff_2" class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/02-logo.png" >
-      <img id="best_medical_staff_3" class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/03-logo.png" >
+      <img onclick="myFunction1()" id="click_me_btn" class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/01-logo.png" >
+      <img onclick="myFunction2()" id="click_me_btn2" class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/02-logo.png" >
+      <img onclick="myFunction3()" id="click_me_btn3" class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/03-logo.png" >
     </div>
     <div class="flex">
-      <img id="best_medical_staff_4" class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/04-logo.png" >
-      <img id="best_medical_staff_5" class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/05-logo.png" >
-      <img id="best_medical_staff_6"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/06-logo.png" >
+      <img onclick="myFunction4()" id="click_me_btn4" class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/04-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/05-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/06-logo.png" >
     </div>
     <div class="flex">
-      <img id="best_medical_staff_7"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/07-logo.png" >
-      <img id="best_medical_staff_8"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/08-logo.png" >
-      <img id="best_medical_staff_9"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/09-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/07-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/08-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/09-logo.png" >
     </div>
     <div class="flex">
-      <img id="best_medical_staff_10"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/10-logo.png" >
-      <img id="best_medical_staff_11"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/11-logo.png" >
-      <img id="best_medical_staff_12"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/12-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/10-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/11-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/12-logo.png" >
     </div>
     <div class="flex">
-      <img id="best_medical_staff_13"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/13-logo.png" >
-      <img id="best_medical_staff_14"  class="mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/14-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/13-logo.png" >
+      <img class="hvr-curl-top-left mx-auto mb-24 lg:h-40 h-24 lg:w-40 w-24" src="<?php bloginfo('template_directory') ?>/img/partners/14-logo.png" >
     </div>
   </div>
   <!-- logo end -->
--- wp-content/themes/brand-h/inc/templates/inquiry.php
+++ wp-content/themes/brand-h/inc/templates/inquiry.php
@@ -2,16 +2,20 @@
 <section class="items-center pt-10">
 
   <!-- Icon (start) -->
-  <div class="mx-auto mb-10 mt-5 lg:h-24 h-24 w-24 lg:w-24"> 
-    <svg class="w-20 h-20" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="tasks" class="svg-inline--fa fa-tasks fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M145.35 207a8 8 0 0 0-11.35 0l-71 71-39-39a8 8 0 0 0-11.31 0L1.35 250.34a8 8 0 0 0 0 11.32l56 56a8 8 0 0 0 11.31 0l88-88a8 8 0 0 0 0-11.32zM62.93 384c-17.67 0-32.4 14.33-32.4 32s14.73 32 32.4 32a32 32 0 0 0 0-64zm82.42-337A8 8 0 0 0 134 47l-71 71-39-39a8 8 0 0 0-11.31 0L1.35 90.34a8 8 0 0 0 0 11.32l56 56a8 8 0 0 0 11.31 0l88-88a8 8 0 0 0 0-11.32zM503 400H199a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h304a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8zm0-320H199a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h304a8 8 0 0 0 8-8V88a8 8 0 0 0-8-8zm0 160H199a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h304a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8z"></path></svg>
+  
+  <div style="margin-top:110px;">
+    <div class="border-gray-700 border w-16 h-16 rounded-full py-2 px-2 flex items-center justify-center text-gray-700" style="margin:0 auto">
+        <svg class="w-8 h-8" aria-hidden="true" focusable="false" data-prefix="fal" data-icon="tasks" class="svg-inline--fa fa-tasks fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M145.35 207a8 8 0 0 0-11.35 0l-71 71-39-39a8 8 0 0 0-11.31 0L1.35 250.34a8 8 0 0 0 0 11.32l56 56a8 8 0 0 0 11.31 0l88-88a8 8 0 0 0 0-11.32zM62.93 384c-17.67 0-32.4 14.33-32.4 32s14.73 32 32.4 32a32 32 0 0 0 0-64zm82.42-337A8 8 0 0 0 134 47l-71 71-39-39a8 8 0 0 0-11.31 0L1.35 90.34a8 8 0 0 0 0 11.32l56 56a8 8 0 0 0 11.31 0l88-88a8 8 0 0 0 0-11.32zM503 400H199a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h304a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8zm0-320H199a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h304a8 8 0 0 0 8-8V88a8 8 0 0 0-8-8zm0 160H199a8 8 0 0 0-8 8v16a8 8 0 0 0 8 8h304a8 8 0 0 0 8-8v-16a8 8 0 0 0-8-8z"></path></svg>
+      </div>
+    
   </div>
 	<!-- Icon (end) -->
 
 	<!-- Content (start) -->
-	<h2 class="font-bold text-left px-4 font-size-24 font-roboto leading-none text-gray-700" style="font-size:22px">
+	<h2 class="font-bold text-left px-4 font-size-24 leading-none text-gray-700" style="font-size:22px; line-height:1.2">
     Please leave your contact information and inquiry. An answer will be given within 24 hours.
   </h3>
-  <p class="text-bold mt-5 font-bold text-left px-6 font-roboto leading-none" style="color:#2681F3;font-size:18px">
+  <p class="text-bold mt-5 font-bold text-left px-6 leading-none" style="color:#2681F3;font-size:18px">
 	
 		* If you have an urget inquiry please leave it via WhatsApp.
 	</p>
@@ -305,12 +309,12 @@
 		</div>
 		<div class="flex items-center mb-10">
 			<div class="w-5/12">
-				<label class="block font-bold mb-1 mb-0 pr-4">
+				<label class="block font-bold mb-1 mb-0 pr-2">
 					<img class="inline lg:pr-2 pr-2" src="<?php bloginfo('template_directory') ?>/img/check.svg" alt="">
 					Medical Report: <span class="text-red-500">*</span>
 				</label>
 			</div>
-			<div class="w-7/12 flex justify-between items-center">
+			<div class="w-8/12 flex justify-between items-center">
 				<label class="inline font-bold lg:px-5 px-2 flex items-center">
 					<input class="mr-4 " name="mediReport" type="radio" required>
 					<span class="lg:text-2xl text-lg ml-4">
@@ -333,7 +337,7 @@
 				</label>
 			</div>
 			<div class="w-full lg:p-10 px-3 pt-3">
-				<textarea class="border border-gray-500 w-full" rows="4"></textarea>
+				<textarea class="border border-gray-500 w-full" rows="3"></textarea>
 			</div>
 		</div>
 		<div class="items-center mb-10">
@@ -346,7 +350,7 @@
 				</label>
 			</div>
 			<div class="w-full lg:p-10 px-3 pt-5">
-				<textarea class="border border-gray-500 w-full" rows="4"></textarea>
+				<textarea class="border border-gray-500 w-full" rows="3"></textarea>
 			</div>
 		</div>
 
